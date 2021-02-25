40cef39668f2b5a0965ac5e756fc9c2bf3ec1a35
jan <jan@flat.net>
2021-02-25 21:47:54 +0100

    initial commit

---
+++ githubTest/githubTest.ino
@@ -0,0 +1,18 @@
+#include "Arduino.h"
+/*
+ * This is a test for checking in and out of github
+ * for sloeber projects
+ *
+ */
+
+#ifndef MY_DEFINE
+#error "Defines have not correctly been transported"
+#endif
+
+void setup()
+{
+}
+
+void loop()
+{
+}
